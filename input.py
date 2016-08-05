b9a1269298edc4300a67eee7c583c17ca1fc4c5f
EoinNPM <eoinnpm@gmail.com>
2016-08-05 17:28:39 +0100

    main.py and input.py added

---
+++ input.py
@@ -0,0 +1,80 @@
+from os import system
+from functools import reduce
+
+
+def get_input(board, ship_tuple, taken):
+    x_str = ''
+    y_str = ''
+    direction = ''
+    (ship, spaces, abbrev) = ship_tuple
+
+    print('Please place your ' + ship + ' (' + str(spaces) + ') in the format "x y direction".')
+
+    try:
+        [x_str, y_str, direction] = input().lower().split()
+    except ValueError:
+        print('Invalid input. Try again.')
+        get_input(board, ship, taken)
+
+    x = eval(x_str)
+    y = eval(y_str)
+    d = direction[0]
+
+    if not (0 < y <= 10 and 0 < x <= 10):
+        print('Invalid input. Try again.')
+        get_input(board, ship, taken)
+
+    y_prime = 21 - (y*2)
+    if d == 'u':
+        if not reduce(lambda a, b: a or b, [(x, i) in taken for i in range(y, y + spaces)], False):
+            for i in range(0, spaces*2, 2):
+                board[y_prime - i] = board[y_prime - i][:((x - 1) * 5)] \
+                                     + '| ' + abbrev + ' ' \
+                                     + board[y_prime - i][(x * 5):]
+                taken.add((x, y + (i // 2)))
+        else:
+            print('Invalid input. Try again.')
+            get_input(board, ship, taken)
+    elif d == 'd':
+        if not reduce(lambda a, b: a or b, [(x, i) in taken for i in range(y - spaces, y)], False):
+            for i in range(0, spaces*2, 2):
+                board[y_prime + i] = board[y_prime - i][:((x - 1) * 5)] \
+                                     + '| ' + abbrev + ' ' + \
+                                     board[y_prime - i][(x * 5):]
+                taken.add((x, y - (i // 2)))
+        else:
+            print('Invalid input. Try again.')
+            get_input(board, ship, taken)
+    elif d == 'l':
+        if not reduce(lambda a, b: a or b, [(i, y) in taken for i in range(x - spaces, x)], False):
+            board[y_prime] = board[y_prime][:(x - spaces) * 5] \
+                             + (('| ' + abbrev + ' ') * spaces) \
+                             + board[y_prime][(x * 5):]
+            for i in range(x - spaces + 1, x + 1):
+                taken.add((i, y))
+        else:
+            print('Invalid input. Try again.')
+            get_input(board, ship, taken)
+    elif d == 'r':
+        if not reduce(lambda a, b: a or b, [(i, y) in taken for i in range(x, x + spaces)], False):
+            board[y_prime] = board[y_prime][:(x - 1) * 5] \
+                             + (('| ' + abbrev + ' ') * spaces) \
+                             + board[y_prime][5 * (x + spaces - 1):]
+            for i in range(x, x + spaces):
+                taken.add((i, y))
+        else:
+            print('Invalid input. Try again.')
+            get_input(board, ship, taken)
+    else:
+        print('Invalid input. Try again.')
+        get_input(board, ship, taken)
+
+    # clears
+    system('cls')
+
+    for line in board:
+        print(line)
+
+    print(str(taken))
+
+    return board, taken
